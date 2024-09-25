3cbad3738f49bd74694fe7de4f33c6a57ebeaa5b
sufyan-rizvi <sofylaiq@gmail.com>
2024-09-25 10:26:56 +0530

    added jqgrid

--- LoanManagementSystem/Controllers/AdminController.cs
+++ LoanManagementSystem/Controllers/AdminController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Web;
 using System.Web.Mvc;
 using LoanManagementSystem.Data;
+using LoanManagementSystem.Models;
 
 namespace LoanManagementSystem.Controllers
 {
@@ -12,11 +14,69 @@ namespace LoanManagementSystem.Controllers
         // GET: Admin
         public ActionResult Index()
         {
-            using(var s= NhibernateHelper.CreateSession())
-            {
-
-            }
+            
             return View();
+        }
+
+        public ActionResult GetData(int page, int rows, string sidx, string sord, bool _search,
+           string searchField, string searchString, string searchOper)
+        {
+            using (var session = NhibernateHelper.CreateSession())
+            {
+                
+                var detailList = session.Query<LoanOfficer>().ToList();
+
+                //if (_search && searchField == "Name" && searchOper == "eq")
+                //{
+                //    detailList = detailList.Where(q => q.Email == searchString).ToList();
+                //}
+
+
+                //Get total count of records 
+                int totalCount = detailList.Count();
+
+                //Calculate total Pages
+                int totalPages = (int)Math.Ceiling((double)totalCount / rows);
+
+                //switch (sidx)
+                //{
+                //    case "PhoneNumber":
+                //        detailList = sord == "asc" ? detailList.OrderBy(p => p.PhoneNumber).ToList()
+                //            : detailList.OrderByDescending(p => p.PhoneNumber).ToList();
+                //        break;
+
+                //    case "Email":
+                //        detailList = sord == "asc" ? detailList.OrderBy(p => p.Email).ToList()
+                //            : detailList.OrderByDescending(p => p.Email).ToList();
+                //        break;
+
+                //    default:
+                //        break;
+
+                //}
+
+
+
+                var jsonData = new
+                {
+                    total = totalPages,
+                    page,
+                    records = totalCount,
+                    
+                    rows = detailList.Select(p => new
+                    {
+
+                        cell = new string[]
+                        {
+                        p.OfficerId.ToString(),
+                        p.User.FirstName,
+                        p.User.LastName
+                        }
+                    }).Skip((page - 1) * rows).Take(rows).ToArray()
+                };
+                
+                return Json(jsonData, JsonRequestBehavior.AllowGet);
+            }
         }
     }
 }
