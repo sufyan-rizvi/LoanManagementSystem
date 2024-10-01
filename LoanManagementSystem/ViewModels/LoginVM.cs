7529a5d2d92a25acf8c459483abfb8448600e3af
sufyan-rizvi <sofylaiq@gmail.com>
2024-10-01 11:16:41 +0530

    comit

--- LoanManagementSystem/ViewModels/LoginVM.cs
+++ LoanManagementSystem/ViewModels/LoginVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.ServiceModel.Security;
 using System.Web;
@@ -8,7 +9,12 @@ namespace LoanManagementSystem.ViewModels
 {
     public class LoginVM
     {
+        [Required]
+        [Display(Name ="Username")]
         public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "Password")]
         public string Password { get; set; }
         public string LoginType { get; set; }
 
