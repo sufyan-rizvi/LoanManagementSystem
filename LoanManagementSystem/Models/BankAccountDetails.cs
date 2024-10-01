a76f38545dff39532baf33c06f260d1648cdad54
KumarSaroj99 <sarojkumar.panda@aurionpro.com>
2024-10-01 11:20:18 +0530

    sync

--- LoanManagementSystem/Models/BankAccountDetails.cs
+++ LoanManagementSystem/Models/BankAccountDetails.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,10 +9,23 @@ namespace LoanManagementSystem.Models
     public class BankAccountDetails
     {
         public virtual Guid AccountDetailId {  get; set; }
+        [Required]
+        [MaxLength(16)]
+        [Display(Name ="Card Number")]
         public virtual int CardNumber {  get; set; }
+        [Required]
+        [Display(Name ="Card Name")]
         public virtual string NameOnCard {  get; set; }
+        [Required]
+        [MaxLength(2)]
+        [Display(Name ="Expiry Month")]
         public virtual string ExpiryMonth {  get; set; }
+        [Required]
+        [MaxLength(2)]
+        [Display(Name ="Expiry Year")]
         public virtual string ExpiryYear {  get; set; }
+        [Required]
+        [MaxLength(3)]
         public virtual string CVV {  get; set; }
         public virtual Customer Customer { get; set; }
 
