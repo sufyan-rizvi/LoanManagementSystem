e6df0fec87cfb8934795973c7284443da5b05dad
KumarSaroj99 <sarojkumar.panda@aurionpro.com>
2024-10-01 11:50:50 +0530

    ghdx

--- LoanManagementSystem/Models/Startup.cs
+++ LoanManagementSystem/Models/Startup.cs
@@ -12,13 +12,13 @@ namespace LoanManagementSystem.Models
 {
     public class Startup
     {
-        public void Configuration(IAppBuilder app)
-        {
-            JobStorage.Current = new SqlServerStorage(@"Data Source=DESKTOP-646UVFV\SQLEXPRESS;Initial Catalog=LoanDb;Integrated Security=True;Connect Timeout=30;Encrypt=false;");
-            SchedulerController obSchedulerController = new SchedulerController();
-            RecurringJob.AddOrUpdate(() => obSchedulerController.RunSchedulerMethod(), Cron.Minutely);
-            app.UseHangfireServer();
-            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
-        }
+        //public void Configuration(IAppBuilder app)
+        //{
+        //    JobStorage.Current = new SqlServerStorage(@"Data Source=DESKTOP-646UVFV\SQLEXPRESS;Initial Catalog=LoanDb;Integrated Security=True;Connect Timeout=30;Encrypt=false;");
+        //    SchedulerController obSchedulerController = new SchedulerController();
+        //    RecurringJob.AddOrUpdate(() => obSchedulerController.RunSchedulerMethod(), Cron.Minutely);
+        //    app.UseHangfireServer();
+        //    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+        //}
     }
 }
